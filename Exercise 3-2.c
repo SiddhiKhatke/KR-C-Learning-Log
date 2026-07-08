27ec22dd507234c6993421ef6597efe8ecc99310
Siddhi Khatke <siddhikhatke20@gmail.com>
2026-07-08 23:37:00 +0530

    Create Exercise 3-2.c

---
+++ Exercise 3-2.c
@@ -0,0 +1,87 @@
+/*Exercise 3-2. Write a function escape(s,t) that converts characters like newline and tab into
+visible escape sequences like \n and \t as it copies the string t to s. Use a switch. Write a 
+function for the other direction as well, converting escape sequences into the real characters. */
+
+#include <stdio.h>
+
+void escape(char s[],const char t[])
+{
+    int i=0;
+    int j=0;
+
+    while(t[i]!='\0')
+    {
+
+        switch(t[i])
+        {
+
+        case '\n':
+            s[j++]='\\';
+            s[j++]='n';
+            break;
+
+        case '\t':
+            s[j++]='\\';
+            s[j++]='t';
+            break;
+
+        default:
+            s[j++]=t[i];
+        }
+
+        i++;
+    }
+    s[j]='\0';
+}
+
+void unescaped(char s[],const char t[])
+{
+    int i=0;
+    int j=0;
+
+    while(t[i]!='\0')
+    {
+        if(t[i]=='\\')
+        {
+            i++;
+            switch(t[i])
+            {
+            case 'n':
+                s[j++]='\n';
+                break;
+
+            case 't':
+                s[j++]='\t';
+                break;
+
+            default:
+                s[j++]='\\';
+                s[j++]=t[i];
+                break;
+            }
+
+        }
+
+        else
+        {
+            s[j++]=t[i];
+        }
+                i++;
+    }
+    s[j]='\0';
+}
+
+int main()
+{
+    char text[]="Hello,\tWorld!\nC programming\tLanguage";
+    char escaped_text[100];
+    char unescaped_text[100];
+
+    printf("Original text:\n%s\n\n\n",text);
+    escape(escaped_text,text);
+    printf("Escaped text:\n%s\n\n\n",escaped_text);
+    unescaped(unescaped_text,escaped_text);
+    printf("Unescaped text:\n%s\n\n\n",unescaped_text);
+
+    return 0;
+}
